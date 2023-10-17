209f418c6ecc123a58281d573dc61c11a24e09f7
ArturShchuvaylo <arthur.shchuvaylo@gmail.com>
2023-10-17 20:04:44 +0300

    small changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -8,22 +8,20 @@ import ErrorBoundry from "./components/error-boundry/error-boundry";
 import PhonesStoreService from "./service/phones-store-service/phones-store-service";
 import { PhonesStoreServiceProvider } from "./components/phones-store-service-context/phones-store-service.context";
 
-
 import store from "./store";
 
 const phonesStoreService = new PhonesStoreService();
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
     <ErrorBoundry>
       <PhonesStoreServiceProvider value={phonesStoreService}>
-        <Router>
+        <Router basename="/redux-store">
           <App />
         </Router>
       </PhonesStoreServiceProvider>
     </ErrorBoundry>
   </Provider>
-)
+);
